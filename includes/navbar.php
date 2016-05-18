c7ffb33bdf712e78c87a7093ea01d358e5065abc
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 18:55:21 -0500

    fixed missing tag on else statment

--- includes/navbar.php
+++ includes/navbar.php
@@ -17,13 +17,14 @@
         			</ul>
         			<ul class="nav navbar-nav navbar-right">
                       
-                    <?php if( isset( $_SESSION['permission'] )){ ?>
+                    <?php if( isset($_SESSION['permission'])){ ?>
                       <li><a class="" href=""><i class="fa fa-user fa-fw"></i>&nbsp;<?php echo $_SESSION['name']; ?></a></li>
                     <?php } ?>
                     <?php if( isset( $_SESSION['permission'] )){ ?>
                       <li><i class="icon-user"></i><a class="btn" href="logout.php">Logout</a></li>
                     <?php } ?>
-                    else{ ?>
+                    <?php } 
+                      else{ ?>
                       <li><i class="icon-user"></i><a class="btn" href="login.php">Login</a></li>
                       <li><i class=""></i><a class="btn" href="register.php">Register</a></li>
                     <?php } ?>
