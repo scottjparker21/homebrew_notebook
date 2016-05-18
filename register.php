257eb06d12e6653450a80d63c6e78c7c2605bb9e
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 00:50:01 -0500

    testing 2

--- register.php
+++ register.php
@@ -1,90 +1,90 @@
 <?php
 
-    // error_reporting(E_ALL);
-    // error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    error_reporting(E_ALL);
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-    // require_once 'includes/session.php';
-    // require_once 'includes/crud.php';
+    require_once 'includes/session.php';
+    require_once 'includes/crud.php';
     
 
-    // if ( !empty($_POST)) {
-    //     // keep track validation errors
-    //     $usernameError = null;
-    //     $passwordError = null;
-    //     $nameError = null;
-    //     $emailError = null;
+    if ( !empty($_POST)) {
+        // keep track validation errors
+        $usernameError = null;
+        $passwordError = null;
+        $nameError = null;
+        $emailError = null;
          
-    //     // keep track post values
-    //     $username = $_POST['username'];
-    //     $password = $_POST['password'];
-    //     $name = $_POST['name'];
-    //     $email = $_POST['email'];
+        // keep track post values
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
          
-    //     // validate input
-    //     $valid = true;
+        // validate input
+        $valid = true;
 
-    //     if (empty($username)) {
-    //         $usernameError = 'Please enter Username';
-    //         $valid = false;
-    //     }
+        if (empty($username)) {
+            $usernameError = 'Please enter Username';
+            $valid = false;
+        }
          
-    //     if (empty($password)) {
-    //         $passwordError = 'Please enter valid Password';
-    //         $valid = false;
-    //     } 
+        if (empty($password)) {
+            $passwordError = 'Please enter valid Password';
+            $valid = false;
+        } 
 
-    //     if (empty($name)) {
-    //         $nameError = 'Please enter Name';
-    //         $valid = false;
-    //     }
+        if (empty($name)) {
+            $nameError = 'Please enter Name';
+            $valid = false;
+        }
 
-    //     if (empty($email)) {
-    //         $emailError = 'Please enter Email';
-    //         $valid = false;
-    //     }
+        if (empty($email)) {
+            $emailError = 'Please enter Email';
+            $valid = false;
+        }
          
-    //     if ($valid) {
-    //         try {
-    //             $pdo = Database::connect();
-    //             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //             $sql = "INSERT INTO user (username,password,name,email,permission) values(?, ?, ?, ?, ?)";
-    //             $q = $pdo->prepare($sql);
-    //             $q->execute(array($username,$password,$name,$email,2));
+        if ($valid) {
+            try {
+                $pdo = Database::connect();
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = "INSERT INTO user (username,password,name,email,permission) values(?, ?, ?, ?, ?)";
+                $q = $pdo->prepare($sql);
+                $q->execute(array($username,$password,$name,$email,2));
 
-    //             $_SESSION["userid"] = $pdo->lastInsertId();
-    //             $_SESSION["permission"] = 1; 
+                $_SESSION["userid"] = $pdo->lastInsertId();
+                $_SESSION["permission"] = 1; 
 
-    //             Database::disconnect();
-    //             header("Location: index.php");
-    //         } catch (PDOException $e){
-    //             echo $e->getMessage();
-    //             die();
-    //         }
+                Database::disconnect();
+                header("Location: index.php");
+            } catch (PDOException $e){
+                echo $e->getMessage();
+                die();
+            }
             
-    //         $pdo = Database::connect();
-    //         $user = ($_POST["username"]);
-    //         $pass = ($_POST["password"]);
-    //         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //         $sql = "SELECT * FROM user WHERE username = ?";
-    //         $q = $pdo->prepare($sql);
-    //         $q->execute(array($_SESSION["userid"]));
-    //         $data = $q->fetch(PDO::FETCH_ASSOC);
+            $pdo = Database::connect();
+            $user = ($_POST["username"]);
+            $pass = ($_POST["password"]);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT * FROM user WHERE username = ?";
+            $q = $pdo->prepare($sql);
+            $q->execute(array($_SESSION["userid"]));
+            $data = $q->fetch(PDO::FETCH_ASSOC);
 
-    //         // $userid = $data['id'];
-    //         // $username = $data['username'];
-    //         // $name = $data['name'];
-    //         // $permission = $data['permission'];
-    //         // $email = $data['email'];
+            // $userid = $data['id'];
+            // $username = $data['username'];
+            // $name = $data['name'];
+            // $permission = $data['permission'];
+            // $email = $data['email'];
 
-    //         // $_SESSION["userid"] = $id;
-    //         // $_SESSION["username"] = $username;
-    //         // $_SESSION["name"] = $name;
-    //         // $_SESSION["permission"] = $permission;
-    //         // $_SESSION["email"] = $email;
+            // $_SESSION["userid"] = $id;
+            // $_SESSION["username"] = $username;
+            // $_SESSION["name"] = $name;
+            // $_SESSION["permission"] = $permission;
+            // $_SESSION["email"] = $email;
 
-    //         Database::disconnect();
-    //     }
-    // }
+            Database::disconnect();
+        }
+    }
 ?>
 
 <!DOCTYPE html>
