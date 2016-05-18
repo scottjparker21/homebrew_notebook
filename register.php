9c3d898ac5664dacc590088b070cdbcdd8cbaf7d
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 09:55:28 -0500

    testing one form item at a time

--- register.php
+++ register.php
@@ -98,16 +98,21 @@
                         <h3>Create an Account</h3>
                     </div>           
                     <form class="" action="register.php" method="post" enctype="multipart/form-data">
-                      <div class="control-group <?php echo !empty($usernameError)?'error':'';?>">
-                        <label class="control-label">Username</label>
+                      
+                      <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
+                        <label class="control-label">Password</label>
                         <div class="controls">
-                            <input name="username" type="text"  placeholder="username" value="<?php echo !empty($username)?$username:'';?>">
-                            <?php if (!empty($usernameError)): ?>
-                                <span class="help-inline"><?php echo $usernameError;?></span>
-                            <?php endif; ?>
+                            <input name="password" type="text" placeholder="password" value="<?php echo !empty($password)?$password:'';?>">
+                            <?php if (!empty($passwordError)): ?>
+                                <span class="help-inline"><?php echo $passwordError;?></span>
+                            <?php endif;?>
                         </div>
                       </div>
                       
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Create</button>
+                          <a class="btn" href="index.php">Back</a>
+                       </div>
                     </form>
                 </div>
             </div>    
