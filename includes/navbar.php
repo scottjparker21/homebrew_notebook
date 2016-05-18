3e516ec385d87b61536aa5397121d96ab3cecf5b
scottjparker21 <scottjparker21@gmail.com>
2016-05-17 22:38:17 -0500

    removed if statments from navbar to test basic functionality

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,7 +12,10 @@
       		<div class="collapse navbar-collapse" id="myNavbar">
         			<ul class="nav navbar-nav">
              			<li><a href="index.php">Home</a></li>
-              		<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">My Recipes<span class="caret"></span></a>
+              		<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Products<span class="caret"></span></a>
+                			<ul class="dropdown-menu">	
+                					<?php foreach ($categories as $row){?><li id="<?php echo $row['id'];?>"><a href="category.php?catid=<?php echo $row['id'];?>"><?php echo $row['name'];?></a></li><?php }?>		
+                			</ul> 
                   <li><a href=""></a></li>                
         			</ul>
         			<ul class="nav navbar-nav navbar-right">
@@ -21,30 +24,10 @@
                             <input type="text" class="form-control" placeholder="Search" id="search">
                       </div>
                   </form>
-                  <?php if( isset( $_SESSION['permission'] ) && $_SESSION['permission'] == 3 ){ ?>
-                      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-user-plus fa-fw"></i>&nbsp; Admin <span class="caret"></span></a>
-                        <ul class="dropdown-menu"> 
-                            <li><a href="crud/address/index.php"> Address </a></li>
-                            <li><a href="crud/bin/index.php"> Bin </a></li>
-                            <li><a href="crud/category/index.php"> Category </a></li>
-                            <li><a href="crud/customer/index.php"> Customer </a></li>
-                            <li><a href="crud/payment/index.php"> Payment </a></li>
-                            <li><a href="crud/product/index.php"> Product </a></li>
-                            <li><a href="crud/shipment_center/index.php"> Shipment Center </a></li>
-                            <li><a href="crud/subcategory/index.php"> Address </a></li>
-                            <li><a href="crud/tag/index.php"> Tag </a></li>
-                        </ul>
-                  <?php } ?>
-                  <?php if( isset( $_SESSION['permission'] ) && $_SESSION['permission'] == 2 ){ ?>
                       <li><a class="" href="customer.php"><i class="fa fa-user fa-fw"></i>&nbsp;<?php echo $_SESSION['first']; ?></a></li>
-                  <?php } ?>
-                  <?php if( isset( $_SESSION['userid'] ) ){ ?>
-                        <li><i class="icon-user"></i><a class="btn" href="logout.php">Logout</a></li>
-                  <?php } 
-                      else{ ?>
+                      <li><i class="icon-user"></i><a class="btn" href="logout.php">Logout</a></li>
                       <li><i class="icon-user"></i><a class="btn" href="login.php">Login</a></li>
                       <li><i class=""></i><a class="btn" href="register.php">Register</a></li>
-                  <?php } ?>
         		  </ul>
       		</div>
   		</div>
