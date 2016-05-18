31d44e47a279fe84210b22c825f477eb777749f8
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 01:00:15 -0500

    testing

--- register.php
+++ register.php
@@ -107,37 +107,7 @@
                             <?php endif; ?>
                         </div>
                       </div>
-                      <div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
-                        <label class="control-label">Password</label>
-                        <div class="controls">
-                            <input name="password" type="text" placeholder="password" value="<?php echo !empty($password)?$password:'';?>">
-                            <?php if (!empty($passwordError)): ?>
-                                <span class="help-inline"><?php echo $passwordError;?></span>
-                            <?php endif;?>
-                        </div>
-                      </div>
-                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
-                        <label class="control-label">Full Name</label>
-                        <div class="controls">
-                            <input name="name" type="text" placeholder="name" value="<?php echo !empty($name)?$name:'';?>">
-                            <?php if (!empty($nameError)): ?>
-                                <span class="help-inline"><?php echo $nameError;?></span>
-                            <?php endif;?>
-                        </div>
-                      </div>
-                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
-                        <label class="control-label">Email</label>
-                        <div class="controls">
-                            <input name="email" type="text"  placeholder="email" value="<?php echo !empty($email)?$email:'';?>">
-                            <?php if (!empty($emailError)); ?>
-                                <span class="help-inline"><?php echo $emailError;?></span>
-                            <?php endif; ?>
-                        </div>
-                      </div>
-                      <div class="form-actions">
-                          <button type="submit" class="btn btn-success">Create</button>
-                          <a class="btn" href="index.php">Back</a>
-                       </div>
+                      
                     </form>
                 </div>
             </div>    
