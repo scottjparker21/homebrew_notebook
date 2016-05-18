62a18262600cb9f1030ecdc122079f88d21f4e17
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 09:57:03 -0500

    testing all form items besides username

--- register.php
+++ register.php
@@ -108,7 +108,24 @@
                             <?php endif;?>
                         </div>
                       </div>
-                      
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Full Name</label>
+                        <div class="controls">
+                            <input name="name" type="text" placeholder="name" value="<?php echo !empty($name)?$name:'';?>">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+                        <label class="control-label">Email</label>
+                        <div class="controls">
+                            <input name="email" type="text"  placeholder="email" value="<?php echo !empty($email)?$email:'';?>">
+                            <?php if (!empty($emailError)); ?>
+                                <span class="help-inline"><?php echo $emailError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
                       <div class="form-actions">
                           <button type="submit" class="btn btn-success">Create</button>
                           <a class="btn" href="index.php">Back</a>
