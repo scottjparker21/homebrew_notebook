f85a752ad1f05d948e35755aa15af47aec8d7793
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 18:52:35 -0500

    changed home paht to /home

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ var brewApp = angular.module('brewApp', ['ngRoute']);
     brewApp.config(function($routeProvider) {
         $routeProvider
               // route for the home page
-            .when('/', {
+            .when('/home', {
                 templateUrl : 'views/home.php',
                 controller  : 'homeController'
             })
