ec28867ee1ff23ae8df7bda6b8bd30d1c9351f3e
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 18:28:48 -0500

    removed header controller from navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,7 +9,7 @@
           		</button>
         			<!-- <a class="navbar-brand" href="#"><h5> Milwaukee Glassware </h5><img src="MKE Glass" alt="" id="navlogo"></a> -->
       		</div>
-      		<div class="collapse navbar-collapse" ng-controller="HeaderController" id="myNavbar">
+      		<div class="collapse navbar-collapse" id="myNavbar">
         			<ul class="nav navbar-nav">
              			<li><a href="index.php">Home</a></li>
               		<li ng-class="{ active: isActive('/recipes')}"><a href="#/recipes">Recipes</a></li>
