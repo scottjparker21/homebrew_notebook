cff42ce64c49146c5ce1f3ec6d5473fba5bcc510
scottjparker21 <scottjparker21@gmail.com>
2016-05-17 23:44:27 -0500

    commented out php code

--- register.php
+++ register.php
@@ -2,84 +2,85 @@
     require_once 'includes/session.php';
     require_once 'includes/crud.php';
  
-    if ( !empty($_POST)) {
-        // keep track validation errors
-        $usernameError = null;
-        $passwordError = null;
-        $nameError = null;
-        $emailError = null;
+ 
+    // if ( !empty($_POST)) {
+    //     // keep track validation errors
+    //     $usernameError = null;
+    //     $passwordError = null;
+    //     $nameError = null;
+    //     $emailError = null;
          
-        // keep track post values
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $name = $_POST['name'];
-        $email = $_POST['email'];
+    //     // keep track post values
+    //     $username = $_POST['username'];
+    //     $password = $_POST['password'];
+    //     $name = $_POST['name'];
+    //     $email = $_POST['email'];
          
-        // validate input
-        $valid = true;
+    //     // validate input
+    //     $valid = true;
 
-        if (empty($username)) {
-            $firstError = 'Please enter Username';
-            $valid = false;
-        }
+    //     if (empty($username)) {
+    //         $firstError = 'Please enter Username';
+    //         $valid = false;
+    //     }
          
-        if (empty($password)) {
-            $lastError = 'Please enter valid Password';
-            $valid = false;
-        } 
+    //     if (empty($password)) {
+    //         $lastError = 'Please enter valid Password';
+    //         $valid = false;
+    //     } 
 
-        if (empty($name)) {
-            $nameError = 'Please enter Name';
-            $valid = false;
-        }
+    //     if (empty($name)) {
+    //         $nameError = 'Please enter Name';
+    //         $valid = false;
+    //     }
 
-        if (empty($email)) {
-            $emailError = 'Please enter Email';
-            $valid = false;
-        }
+    //     if (empty($email)) {
+    //         $emailError = 'Please enter Email';
+    //         $valid = false;
+    //     }
          
-        if ($valid) {
-            try {
-                $pdo = Database::connect();
-                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                $sql = "INSERT INTO user (username,password,name,email) values(?, ?, ?, ?)";
-                $q = $pdo->prepare($sql);
-                $q->execute(array($username,$password,$name,$email));
+    //     if ($valid) {
+    //         try {
+    //             $pdo = Database::connect();
+    //             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //             $sql = "INSERT INTO user (username,password,name,email) values(?, ?, ?, ?)";
+    //             $q = $pdo->prepare($sql);
+    //             $q->execute(array($username,$password,$name,$email));
 
-                $_SESSION["userid"] = $pdo->lastInsertId();
-                $_SESSION["permission"] = 0; 
+    //             $_SESSION["userid"] = $pdo->lastInsertId();
+    //             $_SESSION["permission"] = 0; 
 
-                Database::disconnect();
-                header("Location: index.php");
-            } catch (PDOException $e){
-                echo $e->getMessage();
-                die();
-            }
+    //             Database::disconnect();
+    //             header("Location: index.php");
+    //         } catch (PDOException $e){
+    //             echo $e->getMessage();
+    //             die();
+    //         }
             
-            $pdo = Database::connect();
-            $user = ($_POST["username"]);
-            $pass = ($_POST["password"]);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = "SELECT * FROM user WHERE username = ?";
-            $q = $pdo->prepare($sql);
-            $q->execute(array($_SESSION["userid"]));
-            $data = $q->fetch(PDO::FETCH_ASSOC);
+    //         $pdo = Database::connect();
+    //         $user = ($_POST["username"]);
+    //         $pass = ($_POST["password"]);
+    //         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //         $sql = "SELECT * FROM user WHERE username = ?";
+    //         $q = $pdo->prepare($sql);
+    //         $q->execute(array($_SESSION["userid"]));
+    //         $data = $q->fetch(PDO::FETCH_ASSOC);
 
-            $userid = $data['id'];
-            $username = $data['username'];
-            $name = $data['name'];
-            $permission = $data['permission'];
-            $email = $data['email'];
+    //         $userid = $data['id'];
+    //         $username = $data['username'];
+    //         $name = $data['name'];
+    //         $permission = $data['permission'];
+    //         $email = $data['email'];
 
-            $_SESSION["userid"] = $id;
-            $_SESSION["username"] = $first;
-            $_SESSION["name"] = $name;
-            $_SESSION["permission"] = $permission;
-            $_SESSION["email"] = $email;
+    //         $_SESSION["userid"] = $id;
+    //         $_SESSION["username"] = $first;
+    //         $_SESSION["name"] = $name;
+    //         $_SESSION["permission"] = $permission;
+    //         $_SESSION["email"] = $email;
 
-            Database::disconnect();
-        }
-    }
+    //         Database::disconnect();
+    //     }
+    // }
 ?>
 
 <!DOCTYPE html>
@@ -125,7 +126,7 @@
                         <label class="control-label">Email:</label>
                         <div class="controls">
                             <input name="email" type="text"  placeholder="Email" value="<?php echo !empty($email)?$email:'';?>">
-                            <?php if (!empty($emailError)): ?>
+                            <?php if (!empty($emailError)); ?>
                                 <span class="help-inline"><?php echo $emailError;?></span>
                             <?php endif; ?>
                         </div>
@@ -133,7 +134,7 @@
                       <div class="form-actions">
                           <button type="submit" class="btn btn-success">Create</button>
                           <a class="btn" href="index.php">Back</a>
-                        </div>
+                       </div>
                     </form>
                 </div>
             </div>    
