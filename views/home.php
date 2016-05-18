a8a45e5b846dddbf087718af0836932a12dac387
scottjparker21 <scottjparker21@gmail.com>
2016-05-18 14:00:38 -0500

    fixed link to main.js

--- views/home.php
+++ views/home.php
@@ -1,9 +1,6 @@
+
+
 <p> {{message}} </p>
 
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
-  <script src="/socket.io/socket.io.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-route.min.js"></script>
-  <script src="/main.js"></script>
-  <script src="src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
+<script src="../assets/js/main.js"></script>
