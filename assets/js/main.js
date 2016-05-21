98b6bf0178a1c333594d6d0452e11f2d18f2ec24
scottjparker21 <scottjparker21@gmail.com>
2016-05-21 12:56:15 -0500

    added error feedback to else as well

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,6 +73,10 @@ var brewApp = angular.module('brewApp', ['ngRoute']);
           
             } else {
               // if successful, bind success message to message
+              $scope.errorName = data.errors.name;
+              $scope.errorStyle = data.errors.style;
+              $scope.errorMaltType = data.errors.maltType;
+              $scope.errorDescription = data.errors.description;
               
               $scope.message = data.message;
             }
