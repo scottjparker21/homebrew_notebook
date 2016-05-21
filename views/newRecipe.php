6a6cadd0192701ec85128ffab6298274c3c41912
scottjparker21 <scottjparker21@gmail.com>
2016-05-21 12:19:20 -0500

    added spans to return error feedback

--- views/newRecipe.php
+++ views/newRecipe.php
@@ -2,7 +2,7 @@
 			<div class="col-lg-12">
 				<center><h1 id="quicksand"> New Brew </h1></center>
 			</div>
-				<div id="feedback" ng-show="feedback">{{ feedback }}</div>
+				<div id="message" ng-show="message">{{ message }}</div>
 				<div class="container">
 
 					
@@ -15,24 +15,28 @@
 						    		<label class="control-label">Name</label>
 						    		<div class="controls">
 						      			<input type="text" name="name" ng-model="formData.name">
+						      			<span class="help-block" ng-show="errorName">{{ errorName }}</span> 
 						    		</div>
 						  		</div>
 						  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorStyle}">
 						    		<label class="control-label">Style</label>
 						    		<div class="controls">
 						      			<input type="text" name="style" ng-model="formData.style">
+						      			<span class="help-block" ng-show="errorName">{{ errorStyle }}</span> 
 						    		</div>
 						  		</div>
 						  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorMaltType }">
 						    		<label class="control-label">Malt Type</label>
 						    		<div class="controls">
 						      			<input type="text" name="maltType" ng-model="formData.maltType">
+						      			<span class="help-block" ng-show="errorName">{{ errorMaltType }}</span> 
 						    		</div>
 						  		</div>
 						  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorDescription }">
 						    		<label class="control-label">Description</label>
 						    		<div class="controls">
-						      			<input type="text" name="description" ng-model="formData.description"
+						      			<input type="text" name="description" ng-model="formData.description">
+						      			<span class="help-block" ng-show="errorName">{{ errorDescription }}</span> 
 						    		</div>
 						  		</div>
 						  		<div class="form-row form-actions control-group">
