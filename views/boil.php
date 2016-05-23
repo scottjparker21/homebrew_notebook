68f7ac2d86bf81639ebe9895088ee1825696664c
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 17:36:17 -0500

    removed value from duration for further testing

--- views/boil.php
+++ views/boil.php
@@ -34,7 +34,7 @@
 				  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorduration }">
 				    		<label class="control-label">Duration</label>
 				    		<div class="controls">
-				      			<input value="1" type="text" id="duration" ng-model="formData.duration">
+				      			<input type="text" id="duration" ng-model="formData.duration">
 				      			<span class="help-block" ng-show="!errorDuration">{{ errorDuration }}</span> 
 				    		</div>
 				  		</div>
