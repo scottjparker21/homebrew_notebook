cc18e12158abcb84941adc18c7241e9601183a06
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 16:22:42 -0500

    trying with get element by id

--- views/boil.php
+++ views/boil.php
@@ -35,7 +35,7 @@
 				?>
 				console.log(read);
 				console.log(read[0].duration);
-				
+				document.getElementById("duration").value = read[0].duration;
 			</script>
 		
 			<form class="form-register form-signin" name="boilForm" ng-submit="processForm()" enctype="multipart/form-data">
@@ -47,8 +47,7 @@
 				  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorduration }">
 				    		<label class="control-label">Duration</label>
 				    		<div class="controls">
-				    	<script> document.write('<input value="' + read[0].duration + '">'); </script>
-				      			<!-- <input value="" type="text" ng-model="formData.hops_type"> -->
+				      			<input value="" type="text" id="duration" ng-model="formData.duration">
 				      			<span class="help-block" ng-show="!errorName">{{ errorDuration }}</span> 
 				    		</div>
 				  		</div>
