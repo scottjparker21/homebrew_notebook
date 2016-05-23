e34801c8e1c4e0b1e322d67d5e07fb41b9e0178c
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 13:49:30 -0500

    echo within input tags

--- views/boil.php
+++ views/boil.php
@@ -42,7 +42,7 @@
 				  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorduration }">
 				    		<label class="control-label">Duration</label>
 				    		<div class="controls">
-				      		<?php echo	'<input value="' . $read . '" type="text" name="duration" ng-model="formData.duration">'; ?>
+				      			<input value="<?php echo $read[0]['duration'];?>" type="text" name="duration" ng-model="formData.duration">
 				      			<span class="help-block" ng-show="!errorName">{{ errorDuration }}</span> 
 				    		</div>
 				  		</div>
