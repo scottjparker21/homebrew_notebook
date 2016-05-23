112688e97b50c6b59672b660e679ddb005cd02e1
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 12:11:48 -0500

    updated proper boilUpdate file

--- boilUpdate.php
+++ boilUpdate.php
@@ -3,26 +3,6 @@
 <?php
 
 
-// public function read(){
-// 	try{
-// 		$pdo = Database::connect();
-// 		$sql = 'SELECT * FROM boil WHERE recipe_step_id = ?';
-// 		$q = $pdo->prepare($sql);
-// 		$q->execute(array($_SESSION['rsi']));
-// 		$data = $q->fetchAll(PDO::FETCH_ASSOC);
-// 	    Database::disconnect();
-// 	    return $data;
-// 	} catch (PDOException $error){
-
-// 		header( "Location: 500.php" );
-// 		//echo $error->getMessage();
-// 		die();
-
-// 	}
-
-// }
-
-
 $errors = array();  // array to hold validation errors
 $data = array();        // array to pass back data
 
@@ -44,52 +24,58 @@ $data = array();        // array to pass back data
 // if ( ! empty($errors)) {
 
   // if there are items in our errors array, return those errors
-//   $data['success'] = false;
-//   $data['errors']  = $errors;
+  // $data['success'] = false;
+  // $data['errors']  = $errors;
 
 // }   else {
-// 		$data['errors']= NULL;
+		$data['errors']= NULL;
 		
-		// $duration = $_POST['duration'];
-		// $hops_type = $_POST['hops_type'];
-		// $hops_amt = $_POST['hops_amt'];
-		// $time_added = $_POST['time_added'];
-		// $notes = $_POST['notes'];
+		$duration = $_POST['duration'];
+		$hops_type = $_POST['hops_type'];
+		$hops_amt = $_POST['hops_amt'];
+		$time_added = $_POST['time_added'];
+		$notes = $_POST['notes'];
+
+		$data['duration']= $duration;
+		$data['hops_type']= $hops_type;
+		$data['duration']= $hops_amt;
+		$data['duration']= $time_added;
+		$data['duration']= $notes;
 			
-	 //    // if there are no errors, return a message
-	 //    $data['success'] = true;
-	 //   	$data['message'] = 'Success!';
+	    // if there are no errors, return a message
+	    $data['success'] = true;
+	   	$data['message'] = 'Success!';
 
-		// if (empty($_POST['duration'])) {
-		// 	$_POST['duration'] = NULL;
-		// }
-		// if (empty($_POST['hops_type'])) {
-		// 	$_POST['hops_type'] = NULL;
-		// }
-		// if (empty($_POST['hops_amt'])) {
-		// 	$_POST['hops_amt'] = NULL;
-		// }
-		// if (empty($_POST['time_added'])) {
-		// 	$_POST['time_added'] = NULL;
-		// }
-		// if (empty($_POST['notes'])) {
-		// 	$_POST['notes'] = NULL;
-		// }
-		// // $rsi = $_SESSION["rsi"];
+		if (empty($_POST['duration'])) {
+			$_POST['duration'] = NULL;
+		}
+		if (empty($_POST['hops_type'])) {
+			$_POST['hops_type'] = NULL;
+		}
+		if (empty($_POST['hops_amt'])) {
+			$_POST['hops_amt'] = NULL;
+		}
+		if (empty($_POST['time_added'])) {
+			$_POST['time_added'] = NULL;
+		}
+		if (empty($_POST['notes'])) {
+			$_POST['notes'] = NULL;
+		}
+		// $rsi = $_SESSION["rsi"];
 
-		// $pdo = Database::connect();
+		$pdo = Database::connect();
 
 		         
-        // $pdo = Database::connect();
-        // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // $sql = "UPDATE boil set duration = ?, hops_type = ?, hops_amt = ?, time_added = ?, notes = ? WHERE recipe_step_id = ?";
-        // $q = $pdo->prepare($sql);
-        // $q->execute(array($duration,$hops_type,$hops_amt,$time_added,$notes,2));
-        // Database::disconnect();
-        // header("Location: index.php");
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "UPDATE boil set duration = ?, hops_type = ?, hops_amt = ?, time_added = ?, notes = ? WHERE recipe_step_id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($duration,$hops_type,$hops_amt,$time_added,$notes,2));
+        Database::disconnect();
+        header("Location: index.php");
         
-         // Database::disconnect();
+         Database::disconnect();
     // }
 
     // return a response ==============
-	// echo json_encode($data);
+	echo json_encode($data);
