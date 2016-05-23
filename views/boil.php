c587ce928bee2fb745bd0444541e115ab121ce52
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 15:33:01 -0500

    print array

--- views/boil.php
+++ views/boil.php
@@ -27,7 +27,7 @@
 		
 		<div class="container">
 
-
+			<?php print_r($read); ?>
 		
 			<form class="form-register form-signin" ng-submit="processForm()" enctype="multipart/form-data">
 				<div class="form-register-with-email">
