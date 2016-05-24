76e30bb58d401faae800e646b00c4a2e22d4ba1c
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 21:33:43 -0500

    added link to database file

--- views/recipes.php
+++ views/recipes.php
@@ -1,5 +1,5 @@
 <?php
-
+	require_once '../includes/database.php';
 
 			$pdo = Database::connect();
 			$sql = "SELECT * FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
