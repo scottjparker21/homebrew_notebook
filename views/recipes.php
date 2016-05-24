2a9f80b4b55e6b11046bbb602e6180c470b07056
scottjparker21 <scottjparker21@gmail.com>
2016-05-24 00:45:05 -0500

    referencing as associative array

--- views/recipes.php
+++ views/recipes.php
@@ -11,7 +11,7 @@
 			$data = $q->fetchAll(PDO::FETCH_ASSOC);
 	        Database::disconnect();
 
-	        foreach ($data as $value) {
+	        foreach ($data as $key => $value) {
 
 	        	echo $value;
 	        }
