88316c46d8c03d1cc8810bf3b00039747c9645d9
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 21:42:56 -0500

    trying to foreach over values

--- views/recipes.php
+++ views/recipes.php
@@ -2,7 +2,7 @@
 	require_once '../includes/database.php';
 
 			$pdo = Database::connect();
-			$sql = "SELECT `id` FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
+			$sql = "SELECT * FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
 			$q = $pdo->prepare($sql);
 			$q->execute(array(1));
 			$data = $q->fetchAll(PDO::FETCH_ASSOC);
@@ -11,7 +11,7 @@
 	        print_r($data);
 
 	        foreach($data as $value){
-	        	echo "value" . $value;
-	        }
+	        	echo $value;
+	       	}
 ?>
  
