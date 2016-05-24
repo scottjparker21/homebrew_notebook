2a5cfeb869f4e7cdebb56424f3254714c7876d03
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 21:38:06 -0500

    trying to iterate over ids

--- views/recipes.php
+++ views/recipes.php
@@ -2,12 +2,16 @@
 	require_once '../includes/database.php';
 
 			$pdo = Database::connect();
-			$sql = "SELECT * FROM `recipe_id` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
+			$sql = "SELECT `id` FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
 			$q = $pdo->prepare($sql);
 			$q->execute(array(1));
 			$data = $q->fetchAll(PDO::FETCH_ASSOC);
 	        Database::disconnect();
 
 	        print_r($data);
+
+	        foreach($data as $value){
+	        	echo "value" . $value;
+	        }
 ?>
  
