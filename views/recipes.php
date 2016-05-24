7e5530785377a4d8df091d8cee716e4f9f2ce092
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 21:35:21 -0500

    will it work querying recipe_id?

--- views/recipes.php
+++ views/recipes.php
@@ -2,7 +2,7 @@
 	require_once '../includes/database.php';
 
 			$pdo = Database::connect();
-			$sql = "SELECT * FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
+			$sql = "SELECT * FROM `recipe_id` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
 			$q = $pdo->prepare($sql);
 			$q->execute(array(1));
 			$data = $q->fetchAll(PDO::FETCH_ASSOC);
