47801274c345a043a0593ceb6bff74242fd823f8
scottjparker21 <scottjparker21@gmail.com>
2016-05-23 21:32:21 -0500

    testing inner join

--- views/recipes.php
+++ views/recipes.php
@@ -1,1 +1,13 @@
-<p> {{ message }} </p>
+<?php
+
+
+			$pdo = Database::connect();
+			$sql = "SELECT * FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
+			$q = $pdo->prepare($sql);
+			$q->execute(array(1));
+			$data = $q->fetchAll(PDO::FETCH_ASSOC);
+	        Database::disconnect();
+
+	        print_r($data);
+?>
+ 
