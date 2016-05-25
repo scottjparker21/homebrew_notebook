5abc572c60d95d1de3acffe492dec7208b735912
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 18:46:42 -0500

    making sure view was working prior to includes

--- views/hops.php
+++ views/hops.php
@@ -1,6 +1,5 @@
 <?php 
 		require_once '../includes/database.php';
-		require_once '../includes/rec_step.php';
 
 		// public function read() {
 		// try{
@@ -17,9 +16,9 @@
 		// 	die();
 		// }
 
-		    echo "<pre>";
-			print_r($hops);
-			echo "</pre>";
+		 //    echo "<pre>";
+			// print_r($hops);
+			// echo "</pre>";
 
 
 ?>
@@ -27,9 +26,44 @@
 	<div class="col-lg-12">
 		<center><h1 id="quicksand"> Hops </h1></center>
 	</div>	
-		<div class="container">
+		<div class="container" ng-app="">
 			
-			
+			<div class="row">
+				<div id="icon-box" class="col-lg-10 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
+					<center>
+					<div class="col-lg-2 col-lg-offset-1 col-md-2 col-md-offset-1 col-sm-2 col-sm-offset-2">
+						<div class="img-responsive" ng-init="imgsrc1='assets/img/barley_b.svg'" ng-mouseover="imgsrc1='assets/img/barley_c.svg'" ng-mouseout="imgsrc1='assets/img/barley_b.svg'">
+					        <img class="" ng-src="{{imgsrc1}}" />
+					      <center><div><h4>Mash</h4></div></center>
+					    </div>
+					</div>
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+						<div  ng-init="imgsrc2='assets/img/boil_b.svg'" ng-mouseover="imgsrc2='assets/img/boil_c.svg'" ng-mouseout="imgsrc2='assets/img/boil_b.svg'">
+					        <img class="" ng-src="{{imgsrc2}}" style=""/>
+					      <center><div><h4>Boil</h4></div></center>
+					    </div>
+					</div>
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+						<div class="img-responsive" ng-init="imgsrc3='assets/img/hops_b.svg'" ng-mouseover="imgsrc3='assets/img/hops_c.svg'" ng-mouseout="imgsrc3='assets/img/hops_b.svg'">
+					        <img class="" ng-src="{{imgsrc3}}" style=""/>
+					      <center><div><h4>Hops</h4></div></center>
+					    </div>
+					</div>
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+						<div class="img-responsive" ng-init="imgsrc4='assets/img/carboy_b.svg'" ng-mouseover="imgsrc4='assets/img/carboy_c.svg'" ng-mouseout="imgsrc4='assets/img/carboy_b.svg'">
+					        <img class="" ng-src="{{imgsrc4}}" style=""/>
+					      <center><div><h4>Fermentation</h4></div></center>
+					    </div>
+					</div>
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+						<div class="img-responsive" ng-init="imgsrc5='assets/img/bot_b.svg'" ng-mouseover="imgsrc5='assets/img/bot_c.svg'" ng-mouseout="imgsrc5='assets/img/bot_b.svg'">
+					        <img class="" ng-src="{{imgsrc5}}" style=""/>
+					      <center><div><h4>Bottling</h4></div></center>
+					    </div>
+					</div>
+					</center>
+				</div>
+			</div>
 			
 			<form class="form-register form-signin" name="hopsForm" ng-submit="processForm()" enctype="multipart/form-data">
 				<div class="form-register-with-email">
@@ -71,13 +105,13 @@
 				      			<button id="send" type="submit" class="btn btn-success">Save</button>
 				    		</div>
 				  		</div>
-				  		<pre>
+				  		<!-- <pre>
 							{{ formData.name }}
 							{{ formData.amt}}
 							{{ formData.hops_amt }}
 							{{ formData.time_added }}
 							{{ formData.time_added }}
-						</pre>
+						</pre> -->
 				  	</div>	
 				</div>
 		  	</form>	
