be0eb3c4cfa903e6eec1e327d30d8d9e123bb46d
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 12:45:47 -0500

    placing homebrew notebook text over image

--- views/home.php
+++ views/home.php
@@ -1,17 +1,15 @@
 		<div class="jumbotron">
 			<div class="container-fluid">
-				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="">
-	      			<img class="" src="" alt="">
-	  			</div>
+				<div class="row">
+				<div class="col-lg-12">
+						<center><h1 id="quicksand"> Homebrew Notebook </h1></center>
+					</div>
+				</div>
 	  		</div>
 		</div>
 	<div class="container">
 		<div id="texbox">
-			<div class="row">
-				<div class="col-lg-12">
-					<center><h1 id="quicksand"> Homebrew Notebook </h1></center>
-				</div>
-			</div>
+			
 			<div class="row" style="background-color:#c7ad88;">
 				<div class="col-lg-12">
 					<center><img id="process" ng-src="assets/img/process.svg" /></center>
