728ef7201be7b2f1f2a395cf235d8ec82abd420e
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 01:08:28 -0500

    added other screen size offsets and removed all inline css from icons

--- views/hops.php
+++ views/hops.php
@@ -30,33 +30,33 @@
 			<div class="row">
 				<div id="icon-box" class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
 					<center>
-					<div class="col-lg-2 col-lg-offset-1 col-md-2 col-sm-2">
+					<div class="col-lg-2 col-lg-offset-1 col-md-2 col-md-offset-1 col-sm-2 col-sm-offset-1">
 						<div id="bleh" ng-init="imgsrc1='assets/img/barley_b.svg'" ng-mouseover="imgsrc1='assets/img/barley_c.svg'" ng-mouseout="imgsrc1='assets/img/barley_b.svg'">
-					        <img ng-src="{{imgsrc1}}" style="width: 100%; height: auto;"/>
+					        <img ng-src="{{imgsrc1}}" style=""/>
 					      <center><div><h4>Mash</h4></div></center>
 					    </div>
 					</div>
 					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 						<div id="bleh" ng-init="imgsrc2='assets/img/boil_b.svg'" ng-mouseover="imgsrc2='assets/img/boil_c.svg'" ng-mouseout="imgsrc2='assets/img/boil_b.svg'">
-					        <img ng-src="{{imgsrc2}}" style="width: 100%; height: auto;"/>
+					        <img ng-src="{{imgsrc2}}" style=""/>
 					      <center><div><h4>Boil</h4></div></center>
 					    </div>
 					</div>
 					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 						<div id="bleh" ng-init="imgsrc3='assets/img/hops_b.svg'" ng-mouseover="imgsrc3='assets/img/hops_c.svg'" ng-mouseout="imgsrc3='assets/img/hops_b.svg'">
-					        <img ng-src="{{imgsrc3}}" style="width: 100%; height: auto;"/>
+					        <img ng-src="{{imgsrc3}}" style=""/>
 					      <center><div><h4>Hops</h4></div></center>
 					    </div>
 					</div>
 					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 						<div id="bleh" ng-init="imgsrc4='assets/img/carboy_b.svg'" ng-mouseover="imgsrc4='assets/img/carboy_c.svg'" ng-mouseout="imgsrc4='assets/img/carboy_b.svg'">
-					        <img ng-src="{{imgsrc4}}" style="width: 100%; height: auto;"/>
+					        <img ng-src="{{imgsrc4}}" style=""/>
 					      <center><div><h4>Fermentation</h4></div></center>
 					    </div>
 					</div>
 					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 						<div id="bleh" ng-init="imgsrc5='assets/img/bot_b.svg'" ng-mouseover="imgsrc5='assets/img/bot_c.svg'" ng-mouseout="imgsrc5='assets/img/bot_b.svg'">
-					        <img ng-src="{{imgsrc5}}" style="width: 100%; height: auto;"/>
+					        <img ng-src="{{imgsrc5}}" style=""/>
 					      <center><div><h4>Bottling</h4></div></center>
 					    </div>
 					</div>
