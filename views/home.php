cc16df99b2e497d6a5279e9b4c644b37580315c5
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 15:12:16 -0500

    adding stylesheet to view

--- views/home.php
+++ views/home.php
@@ -1,4 +1,5 @@
 		<link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<div class="jumbotron">
 			<div class="container-fluid">
 				<div class="row">
