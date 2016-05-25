022cc4cce3f71390271fb2ff81a391ac7cc8e58e
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 13:33:36 -0500

    styling sub head

--- views/home.php
+++ views/home.php
@@ -9,8 +9,7 @@
 				</div>
 	  		</div>
 		</div>
-		<div class="container-fluid" style="background-color:#935347;">
-			<div class="row">
+			<div id="subhead" class="row" style="background-color:#935347;">
 				<div class="col-lg-12">
 					<div class="col-lg-8 col-lg-offset-2">
 						<center><h1>Learn,create, and share with Homebrew Notebook.</h1></center>
