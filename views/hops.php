a28dd458ab987b641e6f4be6a9bf11526a522ef1
scottjparker21 <scottjparker21@gmail.com>
2016-05-24 23:59:48 -0500

    fixed link to image, testing

--- views/hops.php
+++ views/hops.php
@@ -29,7 +29,7 @@
 			<div class="row">
 				<div class="col-lg-10">
 					<div class="col-lg-3">
-						<div id="bleh" ng-init="imgsrc='assets/image/bot_b.svg'" ng-mouseover="imgsrc='assets/image/bot_c.svg'" ng-mouseout="imgsrc='assets/image/bot_b.svg'">
+						<div id="bleh" ng-init="imgsrc='assets/img/bot_b.svg'" ng-mouseover="imgsrc='assets/img/bot_c.svg'" ng-mouseout="imgsrc='assets/img/bot_b.svg'">
 					        <img ng-src="{{imgsrc}}"/>
 					      <div>Image description</div>
 					    </div>
