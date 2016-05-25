d626f759962a63db455590a4dd1f63cb33110199
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 00:12:35 -0500

    gave each image unique src vars

--- views/hops.php
+++ views/hops.php
@@ -30,32 +30,32 @@
 				<div class="col-lg-10">
 					<center>
 					<div class="col-lg-2">
-						<div id="bleh" ng-init="imgsrc='assets/img/barley_b.svg'" ng-mouseover="imgsrc='assets/img/barley_c.svg'" ng-mouseout="imgsrc='assets/img/barley_b.svg'">
-					        <img ng-src="{{imgsrc}}"/>
+						<div id="bleh" ng-init="imgsrc1='assets/img/barley_b.svg'" ng-mouseover="imgsrc1='assets/img/barley_c.svg'" ng-mouseout="imgsrc1='assets/img/barley_b.svg'">
+					        <img ng-src="{{imgsrc1}}"/>
 					      <div>img description</div>
 					    </div>
 					</div>
 					<div class="col-lg-2">
-						<div id="bleh" ng-init="imgsrc='assets/img/boil_b.svg'" ng-mouseover="imgsrc='assets/img/boil_c.svg'" ng-mouseout="imgsrc='assets/img/boil_b.svg'">
-					        <img ng-src="{{imgsrc}}"/>
+						<div id="bleh" ng-init="imgsrc2='assets/img/boil_b.svg'" ng-mouseover="imgsrc2='assets/img/boil_c.svg'" ng-mouseout="imgsrc2='assets/img/boil_b.svg'">
+					        <img ng-src="{{imgsrc2}}"/>
 					      <div>img description</div>
 					    </div>
 					</div>
 					<div class="col-lg-2">
-						<div id="bleh" ng-init="imgsrc='assets/img/hops_b.svg'" ng-mouseover="imgsrc='assets/img/hops_c.svg'" ng-mouseout="imgsrc='assets/img/hops_b.svg'">
-					        <img ng-src="{{imgsrc}}"/>
+						<div id="bleh" ng-init="imgsrc3='assets/img/hops_b.svg'" ng-mouseover="imgsrc3='assets/img/hops_c.svg'" ng-mouseout="imgsrc3='assets/img/hops_b.svg'">
+					        <img ng-src="{{imgsrc3}}"/>
 					      <div>img description</div>
 					    </div>
 					</div>
 					<div class="col-lg-2">
-						<div id="bleh" ng-init="imgsrc='assets/img/carboy_b.svg'" ng-mouseover="imgsrc='assets/img/carboy_c.svg'" ng-mouseout="imgsrc='assets/img/carboy_b.svg'">
-					        <img ng-src="{{imgsrc}}"/>
+						<div id="bleh" ng-init="imgsrc4='assets/img/carboy_b.svg'" ng-mouseover="imgsrc4='assets/img/carboy_c.svg'" ng-mouseout="imgsrc4='assets/img/carboy_b.svg'">
+					        <img ng-src="{{imgsrc4}}"/>
 					      <div>img description</div>
 					    </div>
 					</div>
 					<div class="col-lg-2">
-						<div id="bleh" ng-init="imgsrc='assets/img/bot_b.svg'" ng-mouseover="imgsrc='assets/img/bot_c.svg'" ng-mouseout="imgsrc='assets/img/bot_b.svg'">
-					        <img ng-src="{{imgsrc}}"/>
+						<div id="bleh" ng-init="imgsrc5='assets/img/bot_b.svg'" ng-mouseover="imgsrc5='assets/img/bot_c.svg'" ng-mouseout="imgsrc5='assets/img/bot_b.svg'">
+					        <img ng-src="{{imgsrc5}}"/>
 					      <div>img description</div>
 					    </div>
 					</div>
