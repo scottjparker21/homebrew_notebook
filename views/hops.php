23fedff89bb3208106b4af7b07d7af9305a334f0
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 17:24:59 -0500

    changed offset from 1 to 2

--- views/hops.php
+++ views/hops.php
@@ -31,7 +31,7 @@
 			<div class="row">
 				<div id="icon-box" class="col-lg-10 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1">
 					<center>
-					<div class="col-lg-2 col-lg-offset-1 col-md-2 col-md-offset-1 col-sm-2 col-sm-offset-2">
+					<div class="col-lg-2 col-lg-offset-2 col-md-2 col-md-offset-1 col-sm-2 col-sm-offset-2">
 						<div id="step-icon" ng-init="imgsrc1='assets/img/barley_b.svg'" ng-mouseover="imgsrc1='assets/img/barley_c.svg'" ng-mouseout="imgsrc1='assets/img/barley_b.svg'">
 					        <img id="step-icon" ng-src="{{imgsrc1}}" />
 					      <center><div><h4>Mash</h4></div></center>
