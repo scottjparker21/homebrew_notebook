218f4d351cc105001b949d885d5e3cfb69b40a80
scottjparker21 <scottjparker21@gmail.com>
2016-05-24 23:36:06 -0500

    added links to images to be used for app navigation

--- views/hops.php
+++ views/hops.php
@@ -26,6 +26,34 @@
 		<center><h1 id="quicksand"> Hops </h1></center>
 	</div>	
 		<div class="container">
+			<div class="row">
+				<center><div class="col-lg-10">
+					<div class="col-lg-3">
+						<div id="bleh" ng-init="imgsrc='../assets/image/bot_b.svg'" ng-mouseover="imgsrc='../assets/image/bot_c.svg'" ng-mouseout="imgsrc='../assets/image/bot_b.svg'">
+					        <img ng-src="{{imgsrc}}"/>
+					      <div>Image description</div>
+					    </div>
+					</div>
+					<div class="col-lg-3">
+						<div id="bleh" ng-init="imgsrc='../assets/image/bot_b.svg'" ng-mouseover="imgsrc='../assets/image/bot_c.svg'" ng-mouseout="imgsrc='../assets/image/bot_b.svg'">
+					        <img ng-src="{{imgsrc}}"/>
+					      <div>Image description</div>
+					    </div>
+					</div>
+					<div class="col-lg-3">
+						<div id="bleh" ng-init="imgsrc='../assets/image/bot_b.svg'" ng-mouseover="imgsrc='../assets/image/bot_c.svg'" ng-mouseout="imgsrc='../assets/image/bot_b.svg'">
+					        <img ng-src="{{imgsrc}}"/>
+					      <div>Image description</div>
+					    </div>
+					</div>
+					<div class="col-lg-3">
+						<div id="bleh" ng-init="imgsrc='../assets/image/bot_b.svg'" ng-mouseover="imgsrc='../assets/image/bot_c.svg'" ng-mouseout="imgsrc='../assets/image/bot_b.svg'">
+					        <img ng-src="{{imgsrc}}"/>
+					      <div>Image description</div>
+					    </div>
+					</div>
+				</div></center>
+			</div>
 			<form class="form-register form-signin" name="hopsForm" ng-submit="processForm()" enctype="multipart/form-data">
 				<div class="form-register-with-email">
 					<div class="form-white-background">
