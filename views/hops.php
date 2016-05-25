cd906e1ac094fb4ee7870ff833574bc0ea86d491
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 01:01:41 -0500

    stying div containing icons

--- views/hops.php
+++ views/hops.php
@@ -28,7 +28,7 @@
 		<div class="container" ng-app="">
 			
 			<div class="row">
-				<div class="col-lg-10 col-lg-offset-1" style="background-color:floralWhite">
+				<div id="icon-box" class="col-lg-10 col-lg-offset-1" style="background-color:floralWhite">
 					<center>
 					<div class="col-lg-2 col-lg-offset-1 col-md-2 col-sm-2">
 						<div id="bleh" ng-init="imgsrc1='assets/img/barley_b.svg'" ng-mouseover="imgsrc1='assets/img/barley_c.svg'" ng-mouseout="imgsrc1='assets/img/barley_b.svg'">
