a1871880c9478be12ef2091d952574ae7780ae27
scottjparker21 <scottjparker21@gmail.com>
2016-05-26 02:56:24 -0500

    possibly undefined variable breaking

--- views/mash.php
+++ views/mash.php
@@ -7,19 +7,10 @@
 			$pdo = Database::connect();
 			$sql = 'SELECT * FROM mash WHERE recipe_step_id = ?';
 			$q = $pdo->prepare($sql);
-			$q->execute(array($rid));
+			$q->execute(array(3));
 			$mash = $q->fetch(PDO::FETCH_ASSOC);
 		    Database::disconnect();
-		    // return $data;
-		// } catch (PDOException $error){
-		// 	echo "something went wrong.";
-		// 	//echo $error->getMessage();
-		// 	die();
-		// }
 
-		 //    echo "<pre>";
-			// print_r($mash);
-			// echo "</pre>";
 
 
 ?>
