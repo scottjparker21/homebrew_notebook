db9f39acdcaeadca398e801154b9473cdedc8dc6
scottjparker21 <scottjparker21@gmail.com>
2016-05-26 13:16:08 -0500

    gave queries unique ids

--- views/review.php
+++ views/review.php
@@ -1,15 +1,15 @@
 <?php 
 		require_once '../includes/database.php';
-		session_start();
-		$rsi = $_SESSION['rsi'];
+		
+		
 	?>
 
 	<div class="container">
 		<h1> Recipes by: <?php echo $_SESSION['rsi']; ?> </h1>
 
 		<?php
-
-
+			session_start();
+			$rsi = $_SESSION['rsi'];
 			
 
 		//mash
@@ -21,38 +21,38 @@
 		    Database::disconnect();
 
 		//boil
-			$pdo = Database::connect();
-			$sql = 'SELECT * FROM boil WHERE recipe_step_id = ?';
-			$q = $pdo->prepare($sql);
-			$q->execute(array($rsi));
-			$boil = $q->fetch(PDO::FETCH_ASSOC);
+			$pdo2 = Database::connect();
+			$sql2 = 'SELECT * FROM boil WHERE recipe_step_id = ?';
+			$q2 = $pdo2->prepare($sql2);
+			$q2->execute(array($rsi));
+			$boil = $q2->fetch(PDO::FETCH_ASSOC);
 		    Database::disconnect();
 
 		//hops
-			$pdo = Database::connect();
-			$sql = 'SELECT * FROM hops WHERE recipe_step_id = ?';
-			$q = $pdo->prepare($sql);
-			$q->execute(array($rsi));
-			$hops = $q->fetch(PDO::FETCH_ASSOC);
+			$pdo3 = Database::connect();
+			$sql3 = 'SELECT * FROM hops WHERE recipe_step_id = ?';
+			$q3 = $pdo3->prepare($sql3);
+			$q3->execute(array($rsi));
+			$hops = $q3->fetch(PDO::FETCH_ASSOC);
 		    Database::disconnect();
 
 
 
 		//bottling
-			$pdo = Database::connect();
-			$sql = 'SELECT * FROM bottling WHERE recipe_step_id = ?';
-			$q = $pdo->prepare($sql);
-			$q->execute(array($rsi));
-			$bottling = $q->fetch(PDO::FETCH_ASSOC);
+			$pdo4 = Database::connect();
+			$sql4 = 'SELECT * FROM bottling WHERE recipe_step_id = ?';
+			$q4 = $pdo4->prepare($sql4);
+			$q4->execute(array($rsi));
+			$bottling = $q4->fetch(PDO::FETCH_ASSOC);
 		    Database::disconnect();
 
 
 		//fermentation
-		    $pdo = Database::connect();
-			$sql = 'SELECT * FROM fermentation WHERE recipe_step_id = ?';
-			$q = $pdo->prepare($sql);
-			$q->execute(array($rsi));
-			$fermentation = $q->fetch(PDO::FETCH_ASSOC);
+		    $pdo5 = Database::connect();
+			$sql5 = 'SELECT * FROM fermentation WHERE recipe_step_id = ?';
+			$q5 = $pdo5->prepare($sql5);
+			$q5->execute(array($rsi));
+			$fermentation = $q5->fetch(PDO::FETCH_ASSOC);
 		    Database::disconnect();
 
 		    echo $mash['malt_amt'];
