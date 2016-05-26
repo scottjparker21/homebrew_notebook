19895bf38a31f860552240b834d0a9abf4f2f52b
scottjparker21 <scottjparker21@gmail.com>
2016-05-25 19:58:23 -0500

    added outer container

--- views/recipes.php
+++ views/recipes.php
@@ -1,47 +1,44 @@
+	<div class="container">
 
+		<?php
+			require_once '../includes/database.php';
+			session_start();
 
-<?php
-	require_once '../includes/database.php';
-	session_start();
+					$uid = $_SESSION["userid"];
 
-			$uid = $_SESSION["userid"];
-
-			$pdo = Database::connect();
-			$sql = "SELECT `recipe_id` FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
-			$q = $pdo->prepare($sql);
-			$q->execute(array($uid));
-			$data = $q->fetchAll(PDO::FETCH_ASSOC);
-	        Database::disconnect();
-
-	        foreach ($data as $recipe_id => $value) {
-
-	        	foreach ($value as $rid){
-	        		// echo $rid;
-
-	        		$pdo = Database::connect();
-					$sql = "SELECT * FROM `recipe` WHERE `id` = ? ";
+					$pdo = Database::connect();
+					$sql = "SELECT `recipe_id` FROM `recipe` INNER JOIN `recipe_step` ON `recipe`.`id` = `recipe_step`.`recipe_id` WHERE `recipe`.`uid` = ?";
 					$q = $pdo->prepare($sql);
-					$q->execute(array($rid));
-					$recipe = $q->fetch(PDO::FETCH_ASSOC);
-			        
-					$sql2 = "SELECT * FROM `results` WHERE `id` = ? ";
-					$q2 = $pdo->prepare($sql2);
-					$q2->execute(array($rid));
-					$results = $q2->fetch(PDO::FETCH_ASSOC);
+					$q->execute(array($uid));
+					$data = $q->fetchAll(PDO::FETCH_ASSOC);
 			        Database::disconnect();
 
-			        print_r($results);
-			        // echo "name= " . $data2['name'];
-			        echo '<center><div class="user-recipe col-lg-5 col-lg-offset-1">';
-			        echo '<h1>' . $recipe['name'] . '</h1>';
-			        echo '<h3>Style: ' . $recipe['style'] . '</h3>';
-			        echo '<h3>Color: ' . $results['color'] . '</h3>';
-			        echo '</div></center>';
-	        	}
-	        }
+			        foreach ($data as $recipe_id => $value) {
 
+			        	foreach ($value as $rid){
+			        		// echo $rid;
 
-	        
+			        		$pdo = Database::connect();
+							$sql = "SELECT * FROM `recipe` WHERE `id` = ? ";
+							$q = $pdo->prepare($sql);
+							$q->execute(array($rid));
+							$recipe = $q->fetch(PDO::FETCH_ASSOC);
+					        
+							$sql2 = "SELECT * FROM `results` WHERE `id` = ? ";
+							$q2 = $pdo->prepare($sql2);
+							$q2->execute(array($rid));
+							$results = $q2->fetch(PDO::FETCH_ASSOC);
+					        Database::disconnect();
 
-?>
+					        print_r($results);
+					        // echo "name= " . $data2['name'];
+					        echo '<center><div class="user-recipe col-lg-5 col-lg-offset-1">';
+					        echo '<h1>' . $recipe['name'] . '</h1>';
+					        echo '<h3>Style: ' . $recipe['style'] . '</h3>';
+					        echo '<h3>Color: ' . $results['color'] . '</h3>';
+					        echo '</div></center>';
+			        	}
+			        }
+		?>
+	</div>	
  
