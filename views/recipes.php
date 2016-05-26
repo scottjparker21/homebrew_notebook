0f2ec829c6bd4ead926655ae31e5bd8d62dd2ecf
scottjparker21 <scottjparker21@gmail.com>
2016-05-26 17:56:33 -0500

    printing array again

--- views/recipes.php
+++ views/recipes.php
@@ -30,11 +30,10 @@
 							$q3 = $pdo->prepare($sql3);
 							$q3->execute(array($rid));
 							$rsid = $q3->fetchAll(PDO::FETCH_ASSOC);
-							// print_r($rsid);
+							print_r($rsid);
 
-							foreach($rsid as $val=$v){
-								echo $v => $id;
-							}
+							
+
 
 			        		//this returns the data from mash
 			       			//$sql2 = "SELECT * FROM `mash` INNER JOIN `recipe_step` ON `mash`.`recipe_step_id` = `recipe_step`.`id` WHERE `mash`.`recipe_step_id` = ?";
@@ -74,8 +73,7 @@
 					        echo '</div></center>';
 			        	}
 			        	
-			        }
-			        
+			        }	        
 		?>
 
 	</div>	
