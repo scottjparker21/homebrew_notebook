13552a787d90f8946d5f161f25157185772f3dc7
scottjparker21 <scottjparker21@gmail.com>
2016-05-26 00:42:30 -0500

    trying son decode

--- getrid.php
+++ getrid.php
@@ -6,8 +6,13 @@
         // array to pass back data
 $data = array();
 
+$data = json_decode(file_get_contents("php://input"));
+$rid = mysql_real_escape_string($data->rid);
+
+
+
 $_SESSION['rid'] = $_POST['rid'];
-$data['foo'] = $_POST['rid'];
+$data['foo'] = $rid;
 $data['bar'] = $_POST['rid'];
 $data['bar'] = $_POST['obj'];
 $data['sess'] = $_SESSION['rid'] ;
