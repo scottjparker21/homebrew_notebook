020bb7f6598545c79bc95d658064e341984ac65b
scottjparker21 <scottjparker21@gmail.com>
2016-05-26 02:04:54 -0500

    could it have just been the name

--- views/recipes.php
+++ views/recipes.php
@@ -43,13 +43,12 @@
 					        echo '<h1>' . $recipe['name'] . '</h1>';
 					        echo '<h3>Style: ' . $recipe['style'] . '</h3>';
 					        echo '<h3>Color: ' . $results['color'] . '</h3>';
-					        echo '<input  name="rid" ng-init="viewRecipes.rid=' . "'" . $rid . "'" . '" ng-model="viewRecipes.rid">';
+					        echo '<input  ng-init="viewRecipes.rid=' . "'" . $rid . "'" . '" ng-model="viewRecipes.rid">';
 					        echo '<button id="send" type="submit" class="btn btn-success">View/Edit</button>';
 					        echo '</form>';
 					        echo '</div></center>';
-					        echo $rid;
 			        	}
-			        	echo 'outer ' . $rid;
+			        	
 			        }
 			        
 		?>
