902e023cef1fe540117dd214b9a7729249d2c7ad
scottjparker21 <scottjparker21@gmail.com>
2016-05-26 02:48:21 -0500

    putting it above all else

--- includes/session.php
+++ includes/session.php
@@ -3,5 +3,4 @@
 	session_start();
 
 
-	
-​
+?>
--- views/mash.php
+++ views/mash.php
@@ -1,8 +1,6 @@
+<?php require_once '../includes/session.php'; ?>
 <?php 
-
-		require_once '../includes/session.php';
-
-		// public function read() {
+	// public function read() {
 		// try{
 			$rid = $_SESSION['rid'];
 			$pdo = Database::connect();
