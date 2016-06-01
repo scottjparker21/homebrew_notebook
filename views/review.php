a13d649dcf6d23153806e47c8d5d3f5f2f2bad2a
scottjparker21 <scottjparker21@gmail.com>
2016-06-01 11:49:33 -0500

    formatted  review page

--- views/review.php
+++ views/review.php
@@ -6,49 +6,39 @@
 	<div class="container">
 		<h1> Review <?php echo $_SESSION['rsi']; ?> </h1>
 
-		<?php
-			
-			
-		$rsi = $_SESSION['rsi'];
-		//mash
-		$pdo = Database::connect();
-			$sql = 'SELECT * FROM mash WHERE recipe_step_id = ?';
-			$q = $pdo->prepare($sql);
-			$q->execute(array($rsi));
-			$mash = $q->fetch(PDO::FETCH_ASSOC);
-		    Database::disconnect();
+		<?php			
+			$rsi = $_SESSION['rsi'];
+			//mash
+			$pdo = Database::connect();
+				$sql = 'SELECT * FROM mash WHERE recipe_step_id = ?';
+				$q = $pdo->prepare($sql);
+				$q->execute(array($rsi));
+				$mash = $q->fetch(PDO::FETCH_ASSOC);
+			 
+			//boil
+				$sql2 = 'SELECT * FROM boil WHERE recipe_step_id = ?';
+				$q2 = $pdo->prepare($sql2);
+				$q2->execute(array($rsi));
+				$boil = $q2->fetch(PDO::FETCH_ASSOC);
+			//hops
+				$sql3 = 'SELECT * FROM hops WHERE recipe_step_id = ?';
+				$q3 = $pdo->prepare($sql3);
+				$q3->execute(array($rsi));
+				$hops = $q3->fetch(PDO::FETCH_ASSOC);
+			//bottling
+				$sql4 = 'SELECT * FROM bottling WHERE recipe_step_id = ?';
+				$q4 = $pdo->prepare($sql4);
+				$q4->execute(array($rsi));
+				$bottling = $q4->fetch(PDO::FETCH_ASSOC);
+			//fermentation
+				$sql5 = 'SELECT * FROM fermentation WHERE recipe_step_id = ?';
+				$q5 = $pdo->prepare($sql5);
+				$q5->execute(array($rsi));
+				$fermentation = $q5->fetch(PDO::FETCH_ASSOC);
 
-		//boil
-
-			$sql2 = 'SELECT * FROM boil WHERE recipe_step_id = ?';
-			$q2 = $pdo->prepare($sql2);
-			$q2->execute(array($rsi));
-			$boil = $q2->fetch(PDO::FETCH_ASSOC);
-
-		//hops
-			$sql3 = 'SELECT * FROM hops WHERE recipe_step_id = ?';
-			$q3 = $pdo->prepare($sql3);
-			$q3->execute(array($rsi));
-			$hops = $q3->fetch(PDO::FETCH_ASSOC);
-
-
-
-		//bottling
-			$sql4 = 'SELECT * FROM bottling WHERE recipe_step_id = ?';
-			$q4 = $pdo->prepare($sql4);
-			$q4->execute(array($rsi));
-			$bottling = $q4->fetch(PDO::FETCH_ASSOC);
-
-
-		//fermentation
-			$sql5 = 'SELECT * FROM fermentation WHERE recipe_step_id = ?';
-			$q5 = $pdo->prepare($sql5);
-			$q5->execute(array($rsi));
-			$fermentation = $q5->fetch(PDO::FETCH_ASSOC);
-		Database::disconnect();
-
-		    			        
-		?>		<div class="container">
+			Database::disconnect();		    			        
+		?>		
+				<div class="container">
 			<!-- Mash -->
 					<div class="col-lg-6">
 						<form class="" name="mashForm" ng-submit="processForm()" enctype="multipart/form-data">
@@ -201,7 +191,7 @@
 							  		<div class="form-row form-actions control-group">
 							    		<div class="controls">
 						
-							      			<button id="send" type="submit" class="btn btn-success">Save</button>
+							      			<button id="send" type="submit" class="btn btn-success">Update Recipe</button>
 							    		</div>
 							  		</div>
 							  		<!-- <pre>
@@ -255,7 +245,7 @@
 							  		<div class="form-row form-actions control-group">
 							    		<div class="controls">
 						
-							      			<button id="send" type="submit" class="btn btn-success">Next Step</button>
+							      			<button id="send" type="submit" class="btn btn-success">Update Recipe</button>
 							    		</div>
 							  		</div>
 							  		<!-- <pre>
@@ -301,7 +291,7 @@
 							  		<div class="form-row form-actions control-group">
 							    		<div class="controls">
 						
-							      			<button id="send" type="submit" class="btn btn-success">Next Step</button>
+							      			<button id="send" type="submit" class="btn btn-success">Update Recipe</button>
 							    		</div>
 							  		</div>
 							  		<!-- <pre>
