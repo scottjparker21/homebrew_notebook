37af4c649cef8b181c3b8480a07074df239d2c01
scottjparker21 <scottjparker21@gmail.com>
2016-05-31 19:14:39 -0500

    testing fermentation section

--- views/review.php
+++ views/review.php
@@ -102,6 +102,7 @@
 							</div>
 					  	</form>	
 				  	</div>
+
 				  	<div>
 				  		<center><form class="" name="boilForm" ng-submit="processForm()" enctype="multipart/form-data">
 							<div class="">
@@ -160,4 +161,57 @@
 							  	</div>	
 							</div>
 					  	</form></center>
+				  	</div>
+
+				  	<div>
+				  		<form class="form-register form-signin" name="fermentationForm" ng-submit="processForm()" enctype="multipart/form-data">
+							<div class="form-register-with-email">
+								<div class="form-white-background">
+									<!-- success message -->
+									<div id="message" ng-show="message">{{ message }}</div>
+
+							  		<div class="form-title-row control-group" ng-class="{ 'has-error' : erroryeast_type }">
+							    		<label class="control-label">Yeast Type</label>
+							    		<div class="controls">
+							      			<?php echo '<input ng-init="formData.yeast_type=' . "'" . $fermentation['yeast_type'] . "'" . '" type="text"  ng-model="formData.yeast_type">'; ?>
+							      			<span class="help-block" ng-show="!erroryeast_type">{{ erroryeast_type }}</span> 
+							    		</div>
+							  		</div>
+							  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorpitching_temp }">
+							    		<label class="control-label">Pitching Temperature</label>
+							    		<div class="controls">
+							    			<?php echo	'<input ng-init="formData.pitching_temp=' ."'" . $fermentation['pitching_temp'] ."'" . '" type="text" name="pitching_temp" ng-model="formData.pitching_temp">'; ?>
+							      			<span class="help-block" ng-show="!errorpitching_temp">{{ errorpitching_temp }}</span> 
+							    		</div>
+							  		</div>
+							  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errorduration }">
+							    		<label class="control-label">Fermentation Duration</label>
+							    		<div class="controls">
+							    			<?php echo	'<input ng-init="formData.duration=' ."'" . $fermentation['duration'] ."'" . '" type="text" name="duration" ng-model="formData.duration">'; ?>
+							      			<span class="help-block" ng-show="errorduration">{{ errorduration }}</span> 
+							    		</div>
+							  		</div>
+							  		<div class="form-title-row control-group" ng-class="{ 'has-error' : errornotes }">
+							    		<label class="control-label">Notes</label>
+							    		<div class="controls">
+							    			<?php echo	'<input ng-init="formData.notes=' ."'" . $fermentation['notes'] ."'" . '" type="text" name="notes" ng-model="formData.notes">'; ?>
+							      			<span class="help-block" ng-show="errornotes">{{ errornotes }}</span> 
+							    		</div>
+							  		</div>
+							  		<div class="form-row form-actions control-group">
+							    		<div class="controls">
+						
+							      			<button id="send" type="submit" class="btn btn-success">Next Step</button>
+							    		</div>
+							  		</div>
+							  		<!-- <pre>
+										{{ formData.yeast_type }}
+										{{ formData.hops_type}}
+										{{ formData.pitching_temp }}
+										{{ formData.duration }}
+										{{ formData.notes }}
+									</pre> -->
+							  	</div>	
+							</div>
+					  	</form>
 				  	</div>
