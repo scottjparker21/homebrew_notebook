aeabd8dbd7a095cf9397442d42e3295af7fc4487
scottjparker21 <scottjparker21@gmail.com>
2016-06-23 14:25:28 -0500

    fixed mash heading to match others

--- views/mash.php
+++ views/mash.php
@@ -23,7 +23,7 @@
 ?>
 	<div id="mash-background">
 		<div class="col-lg-12">
-			<center><h1 class="pacifico"> Mash </h1></center>
+			<center><h1 class="pacifico-jumbo animated fadeInLeft"> Mash </h1></center>
 		</div>	
 		<div class="container">
 
