4f8021983f4f725e5c11311747c7add25ee3ce9d
scottjparker21 <scottjparker21@gmail.com>
2016-06-25 12:17:02 -0500

    added missing img tag

--- views/home.php
+++ views/home.php
@@ -75,19 +75,19 @@
 						$q2->execute(array($urid));
 						$ur = $q2->fetch(PDO::FETCH_ASSOC);
 
-						echo '<p>' . $ur['name'] . '</p>';
-						echo '<p>' . $ur['style'] . '</p>';
-						echo '<p>' . $ur['malt_type'] . '</p>';	
+						// echo '<p>' . $ur['name'] . '</p>';
+						// echo '<p>' . $ur['style'] . '</p>';
+						// echo '<p>' . $ur['malt_type'] . '</p>';	
 						echo '<div class="row">';
-						echo '<ng-src="assets/img/white_bot_icon.svg">';
+						echo '<img ng-src="assets/img/white_bot_icon.svg">';
 						echo 	'<div class="col-lg-3 user-recipe-box">';
 						echo     	'<h2>' . $ur['name'] . '</h2>';			
 						echo '</div>';
 						echo	'<div class="col-lg-3 user-recipe-box">';
-						echo     	'<h2>' . $ur['name'] . '</h2>';
+						echo     	'<h2>' . $ur['style'] . '</h2>';
 						echo '</div>';
 						echo	'<div class="col-lg-3 user-recipe-box">';
-						echo     	'<h2>' . $ur['name'] . '</h2>';
+						echo     	'<h2>' . $ur['malt_type'] . '</h2>';
 						echo	'</div>';
 						echo '</div>';
 					}
