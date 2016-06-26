72e021d5767c14d1497f547d60bcebbcdcf4747e
scottjparker21 <scottjparker21@gmail.com>
2016-06-26 17:28:12 -0500

    styling boxes on user recipe page

--- views/recipes.php
+++ views/recipes.php
@@ -3,13 +3,11 @@
 		session_start();
 	?>
 
-	<div class="container">
+	<div class="container recipes-page">
 		<div id="rid" ng-show="rid">{{ rid }}</div>
 		<h1> Recipes by: <?php echo $_SESSION['username']; ?> </h1>
 
 		<?php
-			
-
 					$uid = $_SESSION["userid"];
 
 					$pdo = Database::connect();
@@ -17,32 +15,17 @@
 					$q = $pdo->prepare($sql);
 					$q->execute(array($uid));
 					$data = $q->fetchAll(PDO::FETCH_ASSOC);
-					// print_r($data);
-			        
-
+					
 			        foreach ($data as $recipe_id => $value) {
-
 			        	foreach ($value as $rid){
-			        		// echo $rid;
-
-
+			     
 							$sql3 = "SELECT `recipe_step`.`id` FROM `recipe_step` INNER JOIN `recipe` ON `recipe_step`.`recipe_id` = `recipe`.`id` WHERE `recipe_step`.`recipe_id` = ?";
 							$q3 = $pdo->prepare($sql3);
 							$q3->execute(array($rid));
 							$rsid = $q3->fetchAll(PDO::FETCH_ASSOC);
 							
-
 							foreach ($rsid as $key=>$value) {
 							  foreach($value as $k=>$v){
-							  	// echo $k;
-							  	// echo $v;
-
-				        		//this returns the data from mash
-				       			//$sql2 = "SELECT * FROM `mash` INNER JOIN `recipe_step` ON `mash`.`recipe_step_id` = `recipe_step`.`id` WHERE `mash`.`recipe_step_id` = ?";
-								// $q2 = $pdo->prepare($sql2);
-								// $q2->execute(array($uid2));
-								// $mash = $q2->fetchAll(PDO::FETCH_ASSOC);
-								// print_r($mash);
 
 								$sql = "SELECT * FROM `recipe` WHERE `id` = ? ";
 								$q = $pdo->prepare($sql);
@@ -57,7 +40,7 @@
 						        Database::disconnect();
 
 						        echo '<form action="getrid.php" method="post">';
-						        echo '<center><div class="user-recipe col-lg-5 col-lg-offset-1">';
+						        echo '<center><div class="user-recipe-box col-lg-5 col-lg-offset-1">';
 						        echo '<h1>' . $recipe['name'] . '</h1>';
 						        echo '<h3>Style: ' . $recipe['style'] . '</h3>';
 						        echo '<h3>Color: ' . $results['color'] . '</h3>';
@@ -73,8 +56,7 @@
 						        echo '</div></center>';
 						    	}
 							}
-			        	}
-			        	
+			        	}		        	
 			        }
 		?>
 
