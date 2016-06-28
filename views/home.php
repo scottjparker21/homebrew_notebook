6543f8c20c321674663e9cae5cf9c424054b0ce1
scottjparker21 <scottjparker21@gmail.com>
2016-06-28 11:11:05 -0500

    trying different center method

--- views/home.php
+++ views/home.php
@@ -106,24 +106,24 @@
 			</div>
 			<div class="row">
 				<center><div>
-					<div class="process-buttons " ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_milling.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
+					<div class="process-buttons col-lg-1 col-lg-offset-3 img-responsive" ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_milling.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
 			        	<div><h5>Milling</h5></div>
 			    	</div>
 
-					<div class="process-buttons " ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_mashing.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
+					<div class="process-buttons col-lg-1 img-responsive" ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_mashing.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
 			        	<div><h5>Mashing</h5></div>
 			    	</div>
-					<div class="process-buttons " ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_boiling.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
+					<div class="process-buttons col-lg-1 img-responsive" ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_boiling.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
 			        	<div><h5>Boiling</h5></div>
 			   	 	</div>
 
-					<div class="process-buttons " ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_filtering.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
+					<div class="process-buttons col-lg-1 img-responsive" ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_filtering.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
 			        	<div><h5>Filtration</h5></div>
 			    	</div>
-					<div class="process-buttons " ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_fermenting.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
+					<div class="process-buttons col-lg-1 img-responsive" ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_fermenting.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
 			        	<div><h5>Fermentation</h5></div>
 			    	</div>
-					<div class="process-buttons " ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_bottling.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
+					<div class="process-buttons col-lg-1 img-responsive" ng-init="imgsrc='assets/img/processing.svg'" ng-mouseover="imgsrc='assets/img/process_bottling.svg'" ng-mouseout="imgsrc='assets/img/processing.svg'">
 			        	<div><h5>Bottling</h5></div>
 			    	</div>
 		    	</div></center>
