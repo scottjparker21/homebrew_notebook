39f034da3858f1a2b6afe9c479ea0d9baef8c681
scottjparker21 <scottjparker21@gmail.com>
2016-06-28 23:53:49 -0500

    added link to user recipes testing linking angular view

--- sitemap.php
+++ sitemap.php
@@ -1,6 +1,10 @@
 
 <div class="container">
 	<a href="index.php"> Index </a>
+	<a href="index.php#/recipes"> User Recipes </a>
+	<a href="index.php"> Index </a>
+	<a href="index.php"> Index </a>
+
 
 </div>
 
